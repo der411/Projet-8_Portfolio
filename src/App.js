d7f034d9035ae6ceb7e8e0a6654c32cadc821602
der411 <vaitilingomvincent@gmail.com>
2024-08-07 15:14:21 +0200

    Modification react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/style.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Error404 from './pages/Error404';
 
