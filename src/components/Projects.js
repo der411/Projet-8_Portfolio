0f06e75c494c2e2e73adaadcf3a71ae682ae984c
der411 <vaitilingomvincent@gmail.com>
2024-08-20 12:37:26 +0200

    Redimensionnement des images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -30,28 +30,20 @@ function Projects( { projects } ) {
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-5 mb-20">
           {projects.map((project) => (
             <div
-            key={project.id}
-            className="bg-custom-gray shadow-md p-5 rounded cursor-pointer relative overflow-hidden bg-cover bg-center min-h-[300px] transition-all duration-300 hover:shadow-lg"
-            style={{
-              backgroundImage: `url(${project.images[0]})`,
-              backgroundPosition: 'top',
-              backgroundSize: 'cover',
-            }}
-            onClick={() => openModal(project)}
-          >
-            <img
-              src={project.images[0]}
-              alt={project.title}
-              loading="lazy"
-              className="absolute inset-0 w-full h-full object-cover"
-              style={{ display: 'none' }} // Masque l'image qui sert juste à utiliser le lazy loading
-            />
-            <div className="absolute inset-0 bg-black bg-opacity-10 dark:bg-opacity-20 flex items-center justify-center transition-all duration-300 hover:bg-opacity-30 dark:hover:bg-opacity-60"></div>
-            <h3 className="absolute bottom-0 left-0 right-0 text-white font-bold z-10 bg-black bg-opacity-20 px-2 py-1">
-              {project.title}
-            </h3>
-          </div>
-          
+              key={project.id}
+              className="bg-custom-gray shadow-md p-5 rounded cursor-pointer relative overflow-hidden bg-cover bg-center min-h-[300px] transition-all duration-300 hover:shadow-lg "
+              style={{
+                backgroundImage: `url(${project.images[0]})`,
+                backgroundPosition: 'top',
+                backgroundSize: 'cover',
+              }}
+              onClick={() => openModal(project)}
+            >
+               <div className="absolute inset-0 bg-black bg-opacity-10 dark:bg-opacity-20 flex items-center justify-center transition-all duration-300 hover:bg-opacity-30 dark:hover:bg-opacity-60"></div>
+              <h3 className="absolute bottom-0 left-0 right-0 text-white font-bold z-10 bg-black bg-opacity-20 px-2 py-1">
+                {project.title}
+              </h3>
+            </div>
           ))}
         </div>
       </section>
